82c143bbf7d1c9a33eef3ce2a2fcb346361de3fe
Vasya-556 <91149467+Vasya-556@users.noreply.github.com>
2022-05-26 00:25:25 +0300

    Add files via upload

---
+++ labs-8.1.5.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int Count(char* str)
+{
+	int k = 0, pos = 0, i=0, j = 0;
+	char* t;
+	for (int l = 0; str[i] != 0; l++)
+		while (t = strchr(str + pos, ','))
+		{
+			i++;
+			pos = t - str + 1;
+			if (i == 3)
+				j = pos;
+		}
+	return j;
+}
+
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	cout << "position ',' = " << Count(str) << endl;
+	return 0;
+}
